a3ad60527f67d1b2d1b80998862cefc7bbe6e02c
Artem Gusev <agusev2311@mail.ru>
2024-09-05 10:03:46 +0300

    First choose

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print("Hello world!")
+import g4f
+
+print("Choose language\n1. English\n2. Русский")
+
+try:
+    language = int(input())
+except:
+    print("There is no such option. I was too lazy to do while, so run the program again!!!")
+    exit(0)
+
+if (not language in [1, 2]):
+    print("There is no such option. I was too lazy to do while, so run the program again!!!")
+    exit(0)
+
+if (language == 1):
+    print("Choose GPT model (3.5 or 4)")
+elif (language == 2):
+    print("Выберете модель GPT (3.5 или 4)")
+
+gpt_model = input()
+
+if (language == 1):
+    if (not gpt_model in ["3.5", "4"]):
+        print("There is no such option. I was too lazy to do while, so run the program again!!!")
+        exit(0)
+elif (language == 2):
+    if (not gpt_model in ["3.5", "4"]):
+        print("Такого варианта нет. Мне было лень делать while, так что запускайте программу снова!!!")
+        exit(0)
+
+
