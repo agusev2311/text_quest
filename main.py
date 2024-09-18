4dcba3a177ccbd65ea444aefb22c4543beed87ee
tyman123qwerty <tyman123qwerty@gmail.com>
2024-09-18 10:19:36 +0300

    сейчас реально добавил картники аоаоаооаоаоао

--- main.py
+++ main.py
@@ -9,6 +9,91 @@ import datetime
 
 import datetime
 
+# код для запроса для фото-нейросети
+
+import time
+import base64
+
+from random import randint as r
+from random import choice as ch
+
+import os
+
+class Text2ImageAPI:
+
+    def __init__(self, url, api_key, secret_key):
+        self.URL = url
+        self.AUTH_HEADERS = {
+            'X-Key': f'Key {api_key}',
+            'X-Secret': f'Secret {secret_key}',
+        }
+
+    def get_model(self):
+        response = requests.get(self.URL + 'key/api/v1/models', headers=self.AUTH_HEADERS)
+        data = response.json()
+        return data[0]['id']
+
+    def generate(self, prompt, model, images=1, width=1024, height=1024):
+        params = {
+            "type": "GENERATE",
+            "numImages": images,
+            "width": width,
+            "height": height,
+            "generateParams": {
+                "query": f"{prompt}"
+            }
+        }
+
+        data = {
+            'model_id': (None, model),
+            'params': (None, json.dumps(params), 'application/json')
+        }
+        response = requests.post(self.URL + 'key/api/v1/text2image/run', headers=self.AUTH_HEADERS, files=data)
+        data = response.json()
+        return data['uuid']
+
+    def check_generation(self, request_id, attempts=10, delay=10):
+        while attempts > 0:
+            response = requests.get(self.URL + 'key/api/v1/text2image/status/' + request_id, headers=self.AUTH_HEADERS)
+            data = response.json()
+            if data['status'] == 'DONE':
+                return data['images']
+
+            attempts -= 1
+            time.sleep(delay)
+def gen_photo(prom, dirr = "res"):
+    api = Text2ImageAPI('https://api-key.fusionbrain.ai/', 'API', 'secret key')
+    # print(api.get_model())
+    model_id = api.get_model()
+    uuid = api.generate(prom, model_id)
+    images = api.check_generation(uuid)
+
+    # Здесь image_base64 - это строка с данными изображения в формате base64
+    image_base64 = images[0]
+
+    # Декодируем строку base64 в бинарные данные
+    image_data = base64.b64decode(image_base64)
+
+    # Открываем файл для записи бинарных данных изображения
+    try:
+        with open(f"{dirr}/{prom.split('.')[0]} _ {r(0, 100000)}.jpg", "wb") as file:
+            file.write(image_data)
+    except:
+        with open(f"{dirr}/{prom.split('.')[0]} _ {r(0, 100000)}.jpg", "w+") as file:
+            file.write(image_data)
+
+# конец
+"""
+zapros = промпт
+
+    try:
+        os.mkdir(os.getcwd().replace("\\", "/") + f'/' + zapros.replace("\n", " ").split(".")[0])
+    except FileExistsError:
+        print('exist')
+
+    gen_photo(zapros.replace("\n", " ")+', реализм, вид от первого лица, без текста', zapros.replace("\n", " ").split(".")[0])
+"""
+
 def check_request_limit(user_id):
     conn = sqlite3.connect('requests.db')
     cursor = conn.cursor()
